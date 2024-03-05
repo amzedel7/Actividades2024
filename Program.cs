3cd5574785782a098eec89aefbec9ab58ab2747f
Elias Rojas <amzedel7@gmail.com>
2024-03-04 21:46:11 -0400

    rojas

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ActividadEnClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string animal = "Murcielago";
+        int contar = 0;
+
+        foreach (char letra in animal)
+        {
+            if (char.IsLetter(letra))
+            {
+                contar++;
+            }
+        } 
+        console.writeline($"hola{contar}");   
+        }   
+    }
+}
